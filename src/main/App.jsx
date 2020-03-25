63bb0174fd9325de2de6c101d2198731c994e0f6
JonatanMarquess <jon.marquess@gmail.com>
2020-03-25 01:00:18 -0300

    Meu primeiro commit neste projeto

---
+++ src/main/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import { BrowserRouter } from 'react-router-dom'
+
+import Routers from './Routes'
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+/* import Home from '../components/home/Home' */
+import Footer from '../components/template/Footer'
+
+export default props =>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routers />
+            <Footer />
+        </div>
+    </BrowserRouter>
